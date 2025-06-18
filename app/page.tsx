f8e1bfd4405e56df777ab5a685b3e97445eea0eb
“Ranko” <rankobrajovic@gmail.com>
2025-06-19 01:16:19 +0200

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import CTABanner from "@/components/cta-banner";
 import FAQ from "@/components/faq";
 import Features from "@/components/features";
 import Footer from "@/components/footer";
-import Hero from "@/components/hero";
+import Hero from "@/components/hero/hero";
 import { Navbar } from "@/components/navbar";
 import Pricing from "@/components/pricing";
 import Testimonials from "@/components/testimonials";
