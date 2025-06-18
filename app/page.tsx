b53d4e263daaeb9ee6054867b75a91ec514c563b
“Ranko” <rankobrajovic@gmail.com>
2025-06-19 01:22:18 +0200

    modified:   app/page.tsx modified:   components/features.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import FAQ from "@/components/faq";
 import Features from "@/components/features";
 import Footer from "@/components/footer";
 import Hero from "@/components/hero/hero";
+import { BackgroundPattern } from "@/components/hero/background-pattern";
 import { Navbar } from "@/components/navbar";
 import Pricing from "@/components/pricing";
 import Testimonials from "@/components/testimonials";
