8ba9187bce16bc20a5e552fa8b73aa508ba93611
“Ranko” <rankobrajovic@gmail.com>
2025-06-19 01:49:25 +0200

    modified:   components/hero/hero.tsx modified:   components/navbar/navbar.tsx

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <div className="min-h-[calc(100vh-6rem)] flex flex-col items-center py-20 px-6">
       <div className="md:mt-6 flex items-center justify-center">
         <div className="text-center max-w-2xl">
-          <Badge className="bg-primary rounded-sm py-1 border-none">
+          <Badge className="bg-primary rounded-md py-1 border-none">
             v1.0.0 is available now! 🚀
           </Badge>
           <h1 className="mt-6 max-w-[20ch] text-3xl xs:text-4xl sm:text-5xl md:text-6xl font-bold !leading-[1.2] tracking-tight">
